562c80babd6eedaab1da612dc11c3ab08a11673b
Jinsong Zhou <51772723+Ason-web@users.noreply.github.com>
2023-02-09 15:55:55 +0800

    Update cifar.py

--- semisup-fixmatch-cifar/dataset/cifar.py
+++ semisup-fixmatch-cifar/dataset/cifar.py
@@ -284,4 +284,4 @@ class CIFAR100SSL(datasets.CIFAR100):
 DATASET_GETTERS = {'cifar10': get_cifar10,
                    'cifar100': get_cifar100,
                    'cifar10_cld_aug': get_cifar10_cld_aug
-                   'get_cifar10_imbalanced': get_cifar10_imbalanced}
+                   'cifar10_imbalanced': get_cifar10_imbalanced}
